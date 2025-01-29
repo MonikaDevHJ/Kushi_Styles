af5f74fe3b77afca4c359a43389e70756e23ef3f
Monika H J <monikahj7@gmail.com>
2025-01-29 13:19:44 +0530

    Making Kushistyles Website

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div className='container'>
+    <div className=''>
+    <p>Kushi_Styles</p>
+
+    </div>
+    
+    </div>
+  )
+}
+
+export default Navbar
