61ccd11ff6d424371c7a4a3b6736343454694fa3
Monika H J <monikahj7@gmail.com>
2025-02-05 18:16:00 +0530

    navbar is fixed in top screen

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,19 +1,15 @@
 import Navbar from "@/components/Navbar";
-import AllIItem from "../components/AllIItem"
+import AllIItem from "../components/AllIItem";
 import Footer from "@/components/Footer";
-import Offersitems from "@/components/Offersitems"
-
+import Offersitems from "@/components/Offersitems";
 
 export default function Home() {
   return (
-   <div className="bg-slate-100 px-6 md:px-12 lg:px-20 py-4">
-
-    <Navbar/>
-
-    <AllIItem/>
-    <Offersitems/>
-    <Footer/>
-
-   </div>
+    <div className="bg-slate-100 px-6 md:px-12 lg:px-20 py-4 pt-[80px]">
+      <Navbar />
+      <AllIItem />
+      <Offersitems />
+      <Footer />
+    </div>
   );
 }
