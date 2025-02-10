0a9d8127b5fa37c82dd6579ff89624aee43e02d5
Monika H J <monikahj7@gmail.com>
2025-02-10 18:30:11 +0530

    making mobiles resppsnoive

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -8,12 +8,12 @@ interface LayoutProps {
 
 const Layout = ({ children }: LayoutProps) => {
   return (
-    <div className="bg-slate-100 min-h-screen flex flex-col">
+    <div className="bg-slate-100 min-h-screen flex flex-col px-4 sm:px-6 md:px-12 lg:px-12 py-2">
       {/* Fixed Navbar */}
       <Navbar />
 
       {/* Ensure Page Content is Below Navbar */}
-      <div className="flex-1 pt-[50px] sm:pt-[90px] md:pt-[100px] px-4 sm:px-6 md:px-12 lg:px-12 py-2">
+      <div className="flex-1 pt-[50px] sm:pt-[90px] md:pt-[100px] px-1 sm:px-6 md:px-12 lg:px-1 py-2">
         {children}
       </div>
 
--- src/pages/Catagories/Mobile.tsx
+++ src/pages/Catagories/Mobile.tsx
@@ -1,12 +1,21 @@
-import React from 'react';
+import React from "react";
 
 const Mobile = () => {
   return (
-    <div className="  bg-red-200 text-black text-3xl font-bold ">
-      <p>Mobilesdfghjs</p>
-      <p>Additional content can be added here...</p>
+    <div className="mt-10 p-4">
+      <div className="grid grid-cols-1 md:grid-cols-3 lg:grid-cols-4 gap-6">
+        {/* Left Column - Filters */}
+        <div className="md:col-span-1 bg-gray-200 p-4">
+          <p className="text-black text-2xl md:text-3xl font-bold">Filters</p>
+        </div>
+
+        {/* Right Column - Mobiles */}
+        <div className="md:col-span-2 lg:col-span-3 bg-gray-200 p-4">
+          <p className="text-black text-2xl md:text-3xl font-bold">Mobiles</p>
+        </div>
+      </div>
     </div>
   );
-}
+};
 
 export default Mobile;
