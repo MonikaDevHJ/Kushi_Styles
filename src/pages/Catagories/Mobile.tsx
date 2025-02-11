c8d3aa52585548432fac2a9ec0f5af25a6b0e1ae
Monika H J <monikahj7@gmail.com>
2025-02-11 18:20:34 +0530

    making filters on Mobiles pages

--- src/pages/Catagories/Mobile.tsx
+++ src/pages/Catagories/Mobile.tsx
@@ -7,6 +7,55 @@ const Mobile = () => {
         {/* Left Column - Filters */}
         <div className="md:col-span-1 bg-gray-200 p-4">
           <p className="text-black text-2xl md:text-3xl font-bold">Filters</p>
+
+          {/* Dropdown For Categories */}
+          <div className="mb-4">
+            <label className="block text-black font-medium">
+              Select Brand:
+            </label>
+            <select name="" id="" className="w-full p-2 border rounded mt-2">
+              <option value="">All Brands</option>
+              <option value="Vivo">Vivo</option>
+              <option value="Iphone">Iphone</option>
+              <option value="OnePlus">OnePlus</option>
+              <option value="Redmi">Redmi</option>
+              <option value="Samsung">Samsung</option>
+              <option value="opppo">opppo</option>
+            </select>
+          </div>
+          {/* Dropdown for Categories */}
+          <div className="">
+            <p className="Select Featuries">Select Features :</p>
+
+            <div className="">
+              <p className=" flex">
+                <input type="checkbox" className="mr-2 accent-blue-500" />
+                <span>Curve Display</span>
+              </p>
+
+              <p className="flex">
+                <input type="checkbox" className="mr-2 accent-blue-500" />
+                <span>AMOLED Display</span>
+              </p>
+
+              <p className="flex">
+                <input type="checkbox" className="mr-2 accent-blue-500" />
+                <span>Fast Charging</span>
+              </p>
+
+              <p className="flex">
+                <input type="checkbox" className="mr-2 accent-blue-500" />
+                <span>120Hz Refresh Rate</span>
+              </p>
+
+              <p className="flex">
+                <input type="checkbox" className="mr-2 accent-blue-500" />
+                <span>Red Color</span>
+              </p>
+
+             
+            </div>
+          </div>
         </div>
 
         {/* Right Column - Mobiles */}
