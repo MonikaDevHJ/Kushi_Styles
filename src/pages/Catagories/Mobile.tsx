c22c4ebfeb4b1f6682be43710edb48d7e8ac94c6
Monika H J <monikahj7@gmail.com>
2025-02-12 18:25:05 +0530

    mobiles

--- src/pages/Catagories/Mobile.tsx
+++ src/pages/Catagories/Mobile.tsx
@@ -1,4 +1,16 @@
 import React from "react";
+import Image from "next/image";
+import vivo1 from "../../assets/vivo1.png";
+import vivo2 from "../../assets/vivo2.png";
+import vivo3 from "../../assets/vivo3.png";
+import vivo4 from "../../assets/vivo4.png";
+import vivo5 from "../../assets/vivo5.png";
+import iphone1 from "../../assets/iphone1.png";
+import iphone2 from "../../assets/iphone2.png";
+import iphone3 from "../../assets/iphone3.png";
+import iphone4 from "../../assets/iphone4.png";
+
+
 
 const Mobile = () => {
   return (
@@ -28,7 +40,6 @@ const Mobile = () => {
             <p className="Select Featuries">Select Features :</p>
 
             <div className="mt-2">
-
               <p className=" flex">
                 <input type="checkbox" className="mr-2 accent-blue-500" />
                 <span>Curve Display</span>
@@ -53,8 +64,6 @@ const Mobile = () => {
                 <input type="checkbox" className="mr-2 accent-blue-500" />
                 <span>Red Color</span>
               </p>
-
-             
             </div>
           </div>
         </div>
@@ -62,6 +71,117 @@ const Mobile = () => {
         {/* Right Column - Mobiles */}
         <div className="md:col-span-2 lg:col-span-3 bg-gray-200 p-4">
           <p className="text-black text-2xl md:text-3xl font-bold">Mobiles</p>
+
+          {/* grid for Mibile cards */}
+
+          <div className="grid grid-cols-1 sm:grid-cols-1 md-grid-cols-3 lg:grid-cols-4 gap-4">
+            {/* Card 1 */}
+
+            <div className="bg-white p-4 shadow-md rounded-lg">
+              <Image
+                src={vivo5}
+                alt="uytf"
+                objectFit="contain"
+                className="rounded-lg shadow-md"
+                width={150}
+                height={150}
+              />
+              <p className="text-center mt-2 font-semibold">Vivo v29e</p>
+            </div>
+
+            <div className="bg-white p-4 shadow-md rounded-lg">
+              <Image
+                src={vivo1}
+                alt="uytf"
+                objectFit="contain"
+                className="rounded-lg shadow-md"
+                width={150}
+                height={150}
+              />
+              <p className="text-center mt-2 font-semibold">Vivo 200</p>
+            </div>
+
+            <div className="bg-white p-4 shadow-md rounded-lg">
+              <Image
+                src={vivo2}
+                alt="uytf"
+                objectFit="contain"
+                className="rounded-lg shadow-md"
+                width={150}
+                height={150}
+              />
+              <p className="text-center mt-2 font-semibold">Vivo 30</p>
+            </div>
+
+            <div className="bg-white p-4 shadow-md rounded-lg">
+              <Image
+                src={vivo3}
+                alt="uytf"
+                objectFit="contain"
+                className="rounded-lg shadow-md"
+                width={150}
+                height={150}
+              />
+              <p className="text-center mt-2 font-semibold">Vivoy2</p>
+            </div>
+            <div className="bg-white p-4 shadow-md rounded-lg">
+              <Image
+                src={vivo4}
+                alt="uytf"
+                objectFit="contain"
+                className="rounded-lg shadow-md"
+                width={150}
+                height={150}
+              />
+              <p className="text-center mt-2 font-semibold">Vivo v209e</p>
+            </div>
+            <div className="bg-white p-4 shadow-md rounded-lg">
+              <Image
+                src={iphone4}
+                alt="uytf"
+                objectFit="contain"
+                className="rounded-lg shadow-md"
+                width={150}
+                height={150}
+              />
+              <p className="text-center mt-2 font-semibold">Vivo v209e</p>
+            </div>
+            <div className="bg-white p-4 shadow-md rounded  -lg">
+              <Image
+                src={iphone3}
+                alt="uytf"
+                objectFit="contain"
+                className="rounded-lg shadow-md"
+                width={150}
+                height={150}
+              />
+              <p className="text-center mt-2 font-semibold">Vivo v209e</p>
+            </div>
+            <div className="bg-white p-4 shadow-md rounded-lg">
+              <Image
+                src={iphone2}
+                alt="uytf"
+                objectFit="contain"
+                className="rounded-lg shadow-md"
+                width={150}
+                height={150}
+              />
+              <p className="text-center mt-2 font-semibold">Vivo v209e</p>
+            </div>
+            <div className="bg-white p-4 shadow-md rounded-lg">
+              <Image
+                src={iphone1}
+                alt="uytf"
+                objectFit="contain"
+                className="rounded-lg shadow-md"
+                width={150}
+                height={150}
+              />
+              <p className="text-center mt-2 font-semibold">Vivo v209e</p>
+            </div>
+         
+         
+          </div>
         </div>
       </div>
     </div>
